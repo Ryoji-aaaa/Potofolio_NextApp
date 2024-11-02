965a738b55a956e9a8ee0d8238d64e458c1b280d
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-11-02 11:55:45 +0900

    nextAuthの型定義の修正

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,22 @@
 // app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions, User as NextAuthUser } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import connectDB from "@/lib/mongodb";
 import { compare } from "bcryptjs";
 import UserModel from "@/lib/UserModels";
 
 interface Credentials {
+  username: string;
   email: string;
   password: string;
 }
 
-interface User {
+interface User extends NextAuthUser {
   id: string;
   email: string;
   username: string;
 }
+
 
 const authorize = async (credentials: Credentials | undefined): Promise<User | null> => {
   if (!credentials) {
@@ -40,10 +42,10 @@ const authorize = async (credentials: Credentials | undefined): Promise<User | n
   return { id: user._id.toString(), email: user.email, username: user.username };
 };
 
-const authOptions = {
+const authOptions :NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      name: "Credentials",
+      // name: "Credentials",
       credentials: {
         email: {
           label: "Email",
@@ -51,6 +53,7 @@ const authOptions = {
           placeholder: "your-email@example.com",
         },
         password: { label: "Password", type: "password" },
+        username: { label: "Username", type: "text" },
       },
       authorize,
     }),
@@ -65,14 +68,18 @@ const authOptions = {
       if (user) {
         token.id = user.id;
         token.email = user.email;
-        token.username = user.username;
+        token.username = (user as User).username;
       }
       return token;
     },
     async session({ session, token }) {
-      session.user.id = token.id;
-      session.user.email = token.email;
-      session.user.username = token.username;
+      if (token) {
+        session.user = {
+          id: token.id ? String(token.id) : "",          
+          email: token.email || "",      
+          username: token.username ? String(token.username) : "" ,
+        };
+      }
       return session;
     },
   },
