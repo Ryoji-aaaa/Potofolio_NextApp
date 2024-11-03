09f0051c999d2eefb8988182fd7aa61cd1326de6
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-11-03 14:50:19 +0900

    使わないsessionの削除

--- src/app/mypage/reservation/delete-reservation/page.tsx
+++ src/app/mypage/reservation/delete-reservation/page.tsx
@@ -4,7 +4,6 @@ import { useSession } from "next-auth/react";
 import DeleteReserveList from "@/app/components/DeleteReserveList";
 
 export default function DeleteReservationPage() {
-  const { data: session } = useSession();
   return (
     <div>
       {/* 動作未確認なので非表示中 */}
