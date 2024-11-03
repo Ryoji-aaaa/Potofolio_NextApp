1220391138df499cd1a857836fd2abe8b1c22f94
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-11-03 11:24:56 +0900

    使わないimportの削除

--- src/app/components/CheckReservation.tsx
+++ src/app/components/CheckReservation.tsx
@@ -5,7 +5,6 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { DateSelectArg } from '@fullcalendar/core';
-import { set } from 'mongoose';
 interface ReservationData {
     username: string;
     email: string;
